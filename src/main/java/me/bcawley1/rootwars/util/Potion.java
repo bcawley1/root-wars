4608bc6db46d0333ffcba53a612c8e77423ab56a
bcawley1 <braydencawley@gmail.com>
2024-04-04 10:47:32 -0600

    Created a potion enum for easy serialization

---
+++ src/main/java/me/bcawley1/rootwars/util/Potion.java
@@ -0,0 +1,46 @@
+package me.bcawley1.rootwars.util;
+
+import org.bukkit.potion.PotionEffectType;
+
+public enum Potion {
+    SPEED(PotionEffectType.SPEED),
+    SLOWNESS(PotionEffectType.SLOW),
+    HASTE(PotionEffectType.FAST_DIGGING),
+    MINING_FATIGUE(PotionEffectType.SLOW_DIGGING),
+    STRENGTH(PotionEffectType.INCREASE_DAMAGE),
+    INSTANT_HEALTH(PotionEffectType.HEAL),
+    INSTANT_DAMAGE(PotionEffectType.HARM),
+    JUMP_BOOST(PotionEffectType.JUMP),
+    NAUSEA(PotionEffectType.CONFUSION),
+    REGENERATION(PotionEffectType.REGENERATION),
+    RESISTANCE(PotionEffectType.DAMAGE_RESISTANCE),
+    FIRE_RESISTANCE(PotionEffectType.FIRE_RESISTANCE),
+    WATER_BREATHING(PotionEffectType.WATER_BREATHING),
+    INVISIBILITY(PotionEffectType.INVISIBILITY),
+    BLINDNESS(PotionEffectType.BLINDNESS),
+    NIGHT_VISION(PotionEffectType.NIGHT_VISION),
+    HUNGER(PotionEffectType.HUNGER),
+    WEAKNESS(PotionEffectType.WEAKNESS),
+    POISON(org.bukkit.potion.PotionEffectType.POISON),
+    WITHER(PotionEffectType.WITHER),
+    HEALTH_BOOST(PotionEffectType.HEALTH_BOOST),
+    ABSORPTION(PotionEffectType.ABSORPTION),
+    SATURATION(PotionEffectType.SATURATION),
+    GLOWING(PotionEffectType.GLOWING),
+    LEVITATION(PotionEffectType.LEVITATION),
+    LUCK(PotionEffectType.LUCK),
+    UNLUCK(PotionEffectType.UNLUCK),
+    SLOW_FALLING(PotionEffectType.SLOW_FALLING),
+    CONDUIT_POWER(PotionEffectType.CONDUIT_POWER),
+    DOLPHINS_GRACE(PotionEffectType.DOLPHINS_GRACE),
+    BAD_OMEN(PotionEffectType.BAD_OMEN),
+    HERO_OF_THE_VILLAGE(PotionEffectType.HERO_OF_THE_VILLAGE),
+    DARKNESS(PotionEffectType.DARKNESS);
+
+
+    final PotionEffectType type;
+    Potion(PotionEffectType type) {
+        this.type = type;
+    }
+
+}
